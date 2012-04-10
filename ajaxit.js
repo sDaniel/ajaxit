3e970a89f4d6ee001fa4188999480d9244e21820
Sebastian Daniel <daniel@wobster.de>
2012-04-10 13:11:37 +0200

    line endings

--- ajaxit.js
+++ ajaxit.js
@@ -6,7 +6,7 @@ document.getElementsByTagName("html")[0].style.marginTop = "-999999px";
         excludeURL: new Array(),
         readyList: null,
         isInit: false,
-        onSuccess: null,
+        onSuccess: null,
         onError: null,
         onStart:null,
         win:(window.document || window),
@@ -269,5 +269,3 @@ document.getElementsByTagName("html")[0].style.marginTop = "-999999px";
     $.ajaxIt = ajaxItMain;
     
 })(jQuery);
-
-
